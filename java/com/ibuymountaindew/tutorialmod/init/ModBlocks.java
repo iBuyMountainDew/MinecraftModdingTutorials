9054652b4327bc88de34695396986273edc3c453
iBuyMountainDew <37771542+iBuyMountainDew@users.noreply.github.com>
2018-05-05 09:55:08 +0100

    Add files via upload

--- java/com/ibuymountaindew/tutorialmod/init/ModBlocks.java
+++ java/com/ibuymountaindew/tutorialmod/init/ModBlocks.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.ibuymountaindew.tutorialmod.objects.blocks.BlockBase;
+import com.ibuymountaindew.tutorialmod.objects.blocks.CringiumBlock;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -12,6 +13,6 @@ public class ModBlocks
 {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 	
-	public static final Block CRINGIUM_BLOCK = new BlockBase("cringium_block", Material.IRON);
+	public static final Block CRINGIUM_BLOCK = new CringiumBlock("cringium_block", Material.IRON);
 	public static final Block EXAMPLE_BLOCK = new BlockBase("example_block", Material.ANVIL);
 }
